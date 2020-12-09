8763d0cb62cd388e35f24eb9dc0d9a78b8d90532
alexander <coding@aggrosoft.de>
2020-12-09 11:16:35 +0100

    add translations

---
+++ Application/views/admin/de/agpopup_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$aLang = array(
+    'charset'                                   => 'UTF-8',
+    'SHOP_MODULE_GROUP_agpopup_settings' => 'Stamm',
+    'SHOP_MODULE_iCookieLifetime' => 'Cookie Dauer in Tagen',
+    'HELP_SHOP_MODULE_iCookieLifetime' => 'Nachdem der Kunde das Popup geschlossen hat wird es erst nach der festgelegten Anzahl Tagen wieder angezeigt',
+    'SHOP_MODULE_aPopupControllers' => 'Auf diesen Seiten anzeigen',
+    'HELP_SHOP_MODULE_aPopupControllers' => 'Geben Sie hier die Controller Namen an auf denen das Popup angezeigt werden soll (start, details, alist etc.), geben Sie * ein um das Popup auf allen Seiten anzuzeigen.',
+    'SHOP_MODULE_sPopupCmsIdent' => 'CMS Ident Popup Inhalt',
+    'HELP_SHOP_MODULE_sPopupCmsIdent' => 'Legen Sie eine CMS Seite an um den Inhalt des Popups zu steuern, geben Sie die ident hier an'
+);
